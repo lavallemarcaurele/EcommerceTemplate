42723504b6a36d30a3b3ca7742eeb30a61f23006
mlavalle <marc-aurele.lavalle@epitech.eu>
2024-03-26 11:40:11 +0100

    add type in project

--- nuxt3/pages/index.vue
+++ nuxt3/pages/index.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-    {{ response.data }}
+  <div v-if="products">
+    <div v-for="product in products" :key="product.id">
+      {{ product.name }}
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { ApiProductProduct } from '../types/strapi/contentTypes';
+
 const { find } = useStrapi()
 
-const response = await find('products')
+const products = ref<ApiProductProduct[]>([]);
+
+const response = await find<ApiProductProduct[]>('products');
+
+products.value = response.data;
+
 </script>
