43454bf180b95375e20431338cb9705848441e10
mlavalle <marc-aurele.lavalle@epitech.eu>
2024-03-26 11:27:38 +0100

    add nuxt strapi modules, env, tailwind

--- nuxt3/nuxt.config.ts
+++ nuxt3/nuxt.config.ts
@@ -1,4 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/strapi'
+  ],
+  strapi: {
+    url: process.env.STRAPI_URL || 'http://localhost:1337',
+    prefix: '/api',
+    admin: '/admin',
+    version: 'v4',
+    cookie: {},
+    cookieName: 'strapi_jwt'
+  }  
 })
--- nuxt3/pages/index.vue
+++ nuxt3/pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    
+    {{ response.data }}
   </div>
 </template>
 
 <script setup lang="ts">
+const { find } = useStrapi()
+
+const response = await find('products')
 </script>
